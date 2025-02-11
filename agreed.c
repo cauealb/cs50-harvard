3d714aa94d5a49480a4e9b9e0e8e1e806cdf5f79
Cauê Alves <cauealvesb4@gmail.com>
2025-02-11 20:54:53 -0300

    Função em C

--- agreed.c
+++ agreed.c
@@ -1,10 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void meow(void);
+
 int main(void)
 {
-    while(true)
+    for(int i = 0; i < 3; i++)
     {
-        printf("meow.\n");
+        meow();
     }
+}
+
+void meow(void)
+{
+    printf("meowww\n");
 }
