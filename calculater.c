f18abc25848a237f561e27809b0d6f4b365f4857
Cauê Alves <cauealvesb4@gmail.com>
2025-02-12 20:40:55 -0300

    retorno em C

--- calculater.c
+++ calculater.c
@@ -1,10 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int calc(void)
+
 int main(void)
 {
     int x = get_int("x: ");;
     int y = get_int("y: ")
 
-    printf("%i\n", x + y);
+    int z = calc(x, y)
+
+    printf("%i\n", z)
+}
+
+int calc(int a, int b)
+{
+    return a + b
 }
