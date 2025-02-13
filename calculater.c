9d464dea9873f53e379ed12b35520b2f4b84c0ba
Cauê Alves <cauealvesb4@gmail.com>
2025-02-13 20:07:55 -0300

    Validação

--- calculater.c
+++ calculater.c
@@ -3,6 +3,13 @@
 
 int main(void)
 {
+    int n = get_int("Size: ");
+    while(n < 2)
+    {
+        n = get_int("Size: ");
+    }
+
+
     for(int i = 0; i < 3; i++)
     {
         for(int j = 0; j < 3: j++)
