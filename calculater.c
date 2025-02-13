c7b890a25ce855987ae0e84bbe809e1329d89bcf
Cauê Alves <cauealvesb4@gmail.com>
2025-02-13 19:38:38 -0300

    # no C

--- calculater.c
+++ calculater.c
@@ -1,19 +1,9 @@
-#include <cs50.h>
 #include <stdio.h>
-
-int calc(void)
 
 int main(void)
 {
-    int x = get_int("x: ");
-    int y = get_int("y: ");
-
-    int z = calc(x, y);
-
-    printf("%i\n", z);
-}
-
-int calc(int a, int b)
-{
-    return a + b;
+    for(int i = 0; i < 3; i++)
+    {
+        printf("###\n")
+    }
 }
